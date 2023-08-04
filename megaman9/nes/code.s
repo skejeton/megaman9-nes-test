a9bb1bb958a7d259cfda31ec259db6a789452611
skejeton <ishidex2@gmail.com>
2023-08-05 03:27:58 +0400

    NES: Tile drawing test

--- megaman9/nes/code.s
+++ megaman9/nes/code.s
@@ -1,5 +1,53 @@
 .SEGMENT "CODE"
 
+render_things:
+    LDA #$20                                    ; high byte of first tile address
+    STA NES_REG_PPU_ADDR                        ;
+    LDA #$30                                    ; low byte of first tile address
+    STA NES_REG_PPU_ADDR                        ; finish loading first tile address into the address register
+    LDA #$01                                    ; load tile (smiley)
+    STA NES_REG_PPU_DATA                        ; store the tile in the nametable
+    LDA #$21                                    ; high byte of first tile address
+    STA NES_REG_PPU_ADDR                        ;
+    LDA #$CE                                    ; low byte of first tile address
+    STA NES_REG_PPU_ADDR                        ; finish loading first tile address into the address register
+    LDA #$02                                    ; load tile
+    STA NES_REG_PPU_DATA                        ; store the tile in the nametable
+    LDA #$03                                    ; load tile
+    STA NES_REG_PPU_DATA                        ; store the tile in the nametable
+    LDA #$04                                    ; load tile
+    STA NES_REG_PPU_DATA                        ; store the tile in the nametable
+    LDA #$05                                    ; load tile
+    STA NES_REG_PPU_DATA                        ; store the tile in the nametable
+    LDA #$06                                    ; load tile
+    STA NES_REG_PPU_DATA                        ; store the tile in the nametable
+    LDA #$21                                    ; high byte of first tile address
+    STA NES_REG_PPU_ADDR                        ;
+    LDA #$C8                                    ; low byte of first tile address
+    STA NES_REG_PPU_ADDR                        ; finish loading first tile address into the address register
+    LDA #$07                                    ; load tile
+    STA NES_REG_PPU_DATA                        ; store the tile in the nametable
+    LDA #$21                                    ; high byte of first tile address
+    STA NES_REG_PPU_ADDR                        ;
+    LDA #$E9                                    ; low byte of first tile address
+    STA NES_REG_PPU_ADDR                        ; finish loading first tile address into the address register
+    LDA #$07                                    ; load tile
+    STA NES_REG_PPU_DATA                        ; store the tile in the nametable
+    LDA #$21                                    ; high byte of first tile address
+    STA NES_REG_PPU_ADDR                        ;
+    LDA #$B7                                    ; low byte of first tile address
+    STA NES_REG_PPU_ADDR                        ; finish loading first tile address into the address register
+    LDA #$07                                    ; load tile
+    STA NES_REG_PPU_DATA                        ; store the tile in the nametable
+    LDA #$21                                    ; high byte of first tile address
+    STA NES_REG_PPU_ADDR                        ;
+    LDA #$D8                                    ; low byte of first tile address
+    STA NES_REG_PPU_ADDR                        ; finish loading first tile address into the address register
+    LDA #$07                                    ; load tile
+    STA NES_REG_PPU_DATA                        ; store the tile in the nametable
+    RTS
+
+
 nes_reset:
     SEI                                         ; disable IRQs
     CLD                                         ; disable decimal mode
@@ -22,15 +70,30 @@ nes_reset:
     STA NES_REG_PPU_ADDR                        ;
     LDA #$00                                    ; low byte of pallete table address
     STA NES_REG_PPU_ADDR                        ; finish loading pallete table address into the address register
+    LDA #$01                                    ; load color (dark blue)
+    STA NES_REG_PPU_DATA                        ; use the color by the pallete
     LDA #$31                                    ; load color (light blue)
+    STA NES_REG_PPU_DATA                        ; use the color by the pallete
+    LDA #$01                                    ; load color (dark blue)
+    STA NES_REG_PPU_DATA                        ; use the color by the pallete
+    LDA #$35                                    ; load color (pink)
     STA NES_REG_PPU_DATA                        ; use the color by the pallete
     LDA #%00011110                              ;
     STA NES_REG_PPU_MASK                        ; enable sprites and tiles, including leftmost 8 pixels
-    :   JMP :-                                  ; stall forever
-
-
-nes_nmi:
-    RTI
+    CLI                                         ; enable IRQs
+    LDA #$80                                    ;
+    STA NES_REG_PPU_CTRL                        ; enable NMI
+    :   BPL :-                                  ; do nothing forever until next frame
+                                                ;
+nes_nmi:                                        ;
+    PHA                                         ;
+    JSR render_things                           ; render things
+    LDA #$00                                    ;
+    STA NES_REG_PPU_SCROLL                      ;
+    LDA #$00                                    ;
+    STA NES_REG_PPU_SCROLL                      ; set scroll (must be done at the end of NMI!)
+    PLA                                         ;
+    RTI                                         ;
 
 nes_irq:
     RTI
--- megaman9/nes/definitions.s
+++ megaman9/nes/definitions.s
@@ -1,6 +1,7 @@
 NES_REG_PPU_CTRL = $2000                        ;
 NES_REG_PPU_MASK = $2001                        ;
 NES_REG_PPU_STAT = $2002                        ;
+NES_REG_PPU_SCROLL = $2005                      ; write 2x times: first X, then Y offset
 NES_REG_PPU_ADDR = $2006                        ;
 NES_REG_PPU_DATA = $2007                        ;
 NES_REG_APU_DMC_FLAGS = $4010                   ; APU Delta Modulation flags
